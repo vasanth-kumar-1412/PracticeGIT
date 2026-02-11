8eb8356a69805feee26b8bd2a15a0159dc22f354
vasanth-kumar-1412 <vasanthkumar14122k@gmail.com>
2026-02-12 00:45:13 +0530

    Initial commit

---
+++ src/com/hex/Array.java
@@ -0,0 +1,17 @@
+package com.hex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array {
+	
+	public static void main(String[] args) {
+		List<String> li = new ArrayList<>();
+		li.add("Honda");
+		li.add("Toyoto");
+		
+		System.out.println(li);
+			
+	}
+
+}
